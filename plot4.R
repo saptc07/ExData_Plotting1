3b774c6dc34676cfc33a36714798d4c94b3a934f
saptc07 <46454646+saptc07@users.noreply.github.com>
2019-03-21 00:28:49 +0530

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,32 +1,23 @@
 plot4 <- function() {
-        ## Aim of this function is to 
-        ## 1. read the household_power_consumption.txt file
-        ## 2. subset for data taken from 2 days: 2007-02-01 and 2007-02-02
-        ## 3. generate 4 plots in 1 space (GAP vs. time, Vol vs. time, submetering vs. time and GRP vs. time)
-        
-        ## Parameters: none
-        ## Assumes household_power_consumption.txt file located in working dir
-        
-        ## read data
+        ## read household_power_consumption data
         powerdata <- read.table("./household_power_consumption.txt", stringsAsFactors = FALSE, header = TRUE, sep =";"  )
         
         ## Create column in table with date and time merged together
         FullTimeDate <- strptime(paste(powerdata$Date, powerdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
         powerdata <- cbind(powerdata, FullTimeDate)
         
-        ## change class of all columns to correct class
+        ## change class of required columns to correct class
         powerdata$Date <- as.Date(powerdata$Date, format="%d/%m/%Y")
         powerdata$Time <- format(powerdata$Time, format="%H:%M:%S")
         powerdata$Global_active_power <- as.numeric(powerdata$Global_active_power)
         powerdata$Global_reactive_power <- as.numeric(powerdata$Global_reactive_power)
         powerdata$Voltage <- as.numeric(powerdata$Voltage)
-        powerdata$Global_intensity <- as.numeric(powerdata$Global_intensity)
         powerdata$Sub_metering_1 <- as.numeric(powerdata$Sub_metering_1)
         powerdata$Sub_metering_2 <- as.numeric(powerdata$Sub_metering_2)
         powerdata$Sub_metering_3 <- as.numeric(powerdata$Sub_metering_3)
         
-        ## subset data from 2007-02-01 and 2007-02-02
-        subsetdata <- subset(powerdata, Date == "2007-02-01" | Date =="2007-02-02")
+        ## subset data from 2007/02/01 and 2007/02/02
+        subsetdata <- subset(powerdata, Date == "2007/02/01" | Date =="2007/02/02")
         
         ## plot the 4 graphs
         png("plot4.png", width=480, height=480)
