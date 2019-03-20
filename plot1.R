0b90531ac764f9d0110163564b9bb6ce023547cd
saptc07 <46454646+saptc07@users.noreply.github.com>
2019-03-21 01:29:52 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -6,11 +6,16 @@ plot1<- function(){
          
         powerdata$Global_active_power <- as.numeric(powerdata$Global_active_power)
         
-        ## subset data from 2007/02/01 and 2007/02/02
+        ## subset data between 2007/02/01 and 2007/02/02
         subsetdata <- subset(powerdata, Date == "2007/02/01" | Date =="2007/02/02")
         
-        ## plot histogram of global active power for those 2 days
+       
+        ## create png file
         png("plot1.png", width=480, height=480)
+        
+        ##create histogram of global active power for those 2 days
         hist(subsetdata$Global_active_power, col="red", border="black", main ="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency")
+        
+        ##close graphic device
         dev.off()
 }
